925038c02fc8b56b2b445fe61eb6105f8b0b8d5b
Abig4il15 <abigailsitumorang68@gmail.com>
2026-05-20 23:42:34 +0700

    Update Streamlit app title and dataset options

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import numpy as np
 
-
-st.title("Hello, Streamlit!")
-st.write("This is a simple Streamlit app.")
-dataset=st.sidebar.selectbox('pilih dataset')
+st.title('Analisis Sentimen Masyarakt terhadap Program MBG')
+st.write("""Metode Support Vector Machine""")
+dataset=st.sidebar.selectbox('pilih dataset', ['Dataset 1', 'Dataset 2', 'Dataset 3'])
+if dataset == 'Dataset 1':
+    st.write("You selected Dataset 1")
+    data = np.random.rand(10, 2)
+    st.line_chart(data)
